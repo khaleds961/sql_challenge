f4eea2523046aa84b19aa503f44cec0b3d656e49
khaled al houssein <khaledalhoussein3@gmail.com>
2021-03-19 23:17:34 +0200

    Basic Queries

---
+++ answer.sh
@@ -0,0 +1,7 @@
+SELECT name from students
+SELECT * from students WHERE Age > 30
+SELECT name from students where Gender='F' AND age>30
+SELECT Points from students where name='Alex'
+INSERT into students (Name,Age,Gender,Points) VALUES ('Khaled','25','M','200')
+update students set Points='350' WHERE name='Basma'
+UPDATE students set Points='150' WHERE name='Alex'
